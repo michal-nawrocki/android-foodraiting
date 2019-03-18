a1c45c31ba8c2d004fbed70150a51303fdcad1c6
michal-nawrocki <michal.m.nawrocki@gmail.com>
2019-03-18 16:09:10 +0000

    Improved layout and added click detection of the BottomNavigationBar

--- app/src/main/java/com/mxn672/foodrating/MainActivity.java
+++ app/src/main/java/com/mxn672/foodrating/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mxn672.foodrating;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +16,39 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private BottomNavigationView bottomNavi;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavi = (BottomNavigationView) findViewById(R.id.bottom_navi);
+        bottomNavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.map:
+                        Toast.makeText(getApplicationContext(), "Action MAP", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.search:
+                        Toast.makeText(getApplicationContext(), "Action SEARCH", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.profile:
+                    Toast.makeText(getApplicationContext(), "Action Profile", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                return true;
+            }
+        });
+
+
+
         recyclerView = (RecyclerView) findViewById(R.id.establishmentList);
         // use this setting to
         // improve performance if you know that changes
@@ -28,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         List<String> input = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 5; i++) {
             input.add("Test" + i);
         }// define an adapter
         mAdapter = new MyAdapter(input);
